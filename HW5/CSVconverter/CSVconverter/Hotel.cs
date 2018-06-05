77e8b970adaa11e6de3dde50274b7df4e2790ae5
Roma Bahlai <feared@ex.ua>
2018-06-06 01:41:28 +0300

    CSV to XML/JSON

---
+++ HW5/CSVconverter/CSVconverter/Hotel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVconverter
+{
+    [Serializable]
+    public class Hotel
+    {
+        public string name;
+        public int id;
+        public string foundedDate;
+        public int capacity;
+        public double rating;
+    }
+}
--- HW5/CSVconverter/CSVconverter/Program.cs
+++ HW5/CSVconverter/CSVconverter/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.Xml;
-using System.Xml.Linq;
+using System.Web.Script.Serialization;
+using System.Xml.Serialization;
 
 namespace CSVconverter
 {
@@ -13,56 +14,112 @@ namespace CSVconverter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
-            //ReadCSV();
-            saveJSON();
+            Menu();
             Console.ReadLine();
         }
 
-        public static XElement ReadCSV()
+        public static void Menu()
         {
-            string csvString = @"Edelweiss;11;5/25/1988;180;4,3;
-Bristol;12;4/3/1965;140;4,1;
-Hilton;1;10/02/1863;321;2,0;
-Balaton;50;5/1/2000;110;;
-Empire;13;7/25/2011;240;5,0;";
-            File.WriteAllText("cust.csv", csvString);
+            string str = Environment.CurrentDirectory;
+            Console.Clear();
+            Console.WriteLine("Convert to: ");
+            Console.WriteLine("1) XML");
+            Console.WriteLine("2) JSON");
+            Console.WriteLine("3) Open folder with converted files");
+            Console.WriteLine("Press 1-3 to select action...");
+            int tmp = Convert.ToInt32(Console.ReadLine());
 
-            string[] source = File.ReadAllLines("cust.csv");
-            XElement hotel = new XElement("Info",
-                from str in source
-                let fields = str.Split(';')
-                select new XElement("Hotel",
-                    new XAttribute("HotelName", fields[0]),
-                    new XElement("HotelId", fields[1]),
-                    new XElement("FoundedDate", fields[2]),
-                    new XElement("TouristCapacity", fields[3]),
-                    new XElement("Rating", fields[4]))
-
-            );
-            //Console.WriteLine(hotel);
-            //cust.Save("xml.xml");
-            return hotel;
-        }
-
-        public static void saveXML(XElement hotel)
-        {
-            
-        }
-
-        public static void saveJSON()
-        {
-            var csv = new List<string[]>();
-            var lines = System.IO.File.ReadAllLines(@"F:\FeareD\GitHub\ATQC\HW5\CSVconverter\CSVconverter\bin\Debug\cust.csv");
-            foreach (string line in lines)
+            switch (tmp)
             {
-                csv.Add(line.Split(';'));
-                Console.WriteLine(line);
+                case (1):
+                    Console.Clear();
+                    ReadCSV(true);
+                    break;
+                case (2):
+                    Console.Clear();
+                    ReadCSV(false);
+                    break;
+                case (3):
+                    Process.Start(str);
+                    Console.Clear();
+                    Menu();
+                    break;
+                default:
+                    Console.WriteLine("Wrong number. Try again? (y/n)");
+                    GetToMenu();
+                    break;
             }
-                      
-            string json = new System.Web.Script.Serialization.JavaScriptSerializer().Serialize(csv);
+        }
+
+        public static void GetToMenu()
+        {
+            bool check = false;
+            while (!check)
+            {
+                string status = Console.ReadLine();
+                if (status == "y")
+                    Menu();
+                else if (status == "n")
+                {
+                    check = true;
+                    Console.WriteLine("See you!");
+                    Environment.Exit(0);
+                    break;
+                }
+            }
+        }
+
+        public static void ReadCSV(bool flag)
+        {
+            string inputCSV = ConfigurationManager.ConnectionStrings["inputCSV"].ConnectionString;
+            StreamReader sr = new StreamReader(inputCSV);
+            String line;
+            List<Hotel> hotelsList = new List<Hotel>();
+            while ((line = sr.ReadLine()) != null)
+            {
+                String[] parts = line.Split(';');
+                Hotel hotel = new Hotel();
+                parts[0] = parts[0].Replace('"', ' ').Trim();
+                hotel.name = parts[0].Trim();
+                hotel.id = Convert.ToInt32(parts[1]);
+                hotel.foundedDate = parts[2].Trim();
+                hotel.capacity = Convert.ToInt32(parts[3]);
+                parts[4] = parts[4].Replace('.', ',').Trim();
+                if (parts[4] == "")
+                    parts[4] = "0";
+                hotel.rating = Convert.ToDouble(parts[4]);
+                hotelsList.Add(hotel);
+            }
+            sr.Close();
+            List<Hotel> SortedList = hotelsList.OrderBy(o => o.rating).ToList();
+            if (flag)
+                saveXML(SortedList);
+            else
+                saveJSON(SortedList);
+        }
+
+        public static void saveXML(List<Hotel> hotels)
+        {
+            StringBuilder output = new StringBuilder();
+            var writer = new StringWriter(output);
+            string outputXML = ConfigurationManager.ConnectionStrings["outputXML"].ConnectionString;
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Hotel>));
+            serializer.Serialize(writer, hotels);
+            Console.WriteLine(output.ToString());
+            System.IO.File.WriteAllText(outputXML, output.ToString());
+            Console.WriteLine("\nBack to menu? y/n");
+            GetToMenu();
+        }
+
+        public static void saveJSON(List<Hotel> hotels)
+        {
+            var jsonSerialiser = new JavaScriptSerializer();
+            var json = jsonSerialiser.Serialize(hotels);
             Console.WriteLine(json);
-            System.IO.File.WriteAllText(@"F:\FeareD\GitHub\ATQC\HW5\CSVconverter\CSVconverter\bin\Debug\json.txt", json);
+            string outputJSON = ConfigurationManager.ConnectionStrings["outputJSON"].ConnectionString;
+            System.IO.File.WriteAllText(outputJSON, json);
+            Console.WriteLine("\nBack to menu? y/n");
+            GetToMenu();
         }
     }
 }
